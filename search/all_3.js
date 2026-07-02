3f82b51f9674fa7d4b1f6ad0c585f9a6d1099bd9
sakamoti <sakamoti@users.noreply.github.com>
2026-07-02 01:36:09 +0000

    deploy: f4503d8ffe416ca983e053ce421f0b20fb4e8e39

--- search/all_3.js
+++ search/all_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['589_0',['Recurrence for degree  \_form#589',['../theory_bsplines.html#autotoc_md22',1,'']]]
+  ['556_0',['Recurrence for degree  \_form#556',['../theory_bsplines.html#autotoc_md22',1,'']]]
 ];
